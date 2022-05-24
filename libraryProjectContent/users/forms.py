1b93b638752b61f767e78605f11ee4a45b60bbc4
Ion Gorincioi <49958966+IonGorincioi@users.noreply.github.com>
2022-05-24 22:19:22 +0100

    models and users form completed

--- libraryProjectContent/users/forms.py
+++ libraryProjectContent/users/forms.py
@@ -2,7 +2,11 @@
 # # Form imports
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from flask_wtf.file import FileAllowed, FileField
+from libraryProjectContent.models import User
+
 
 # ############################################################
 # #############   CREATE THE LOGIN FORM   #############
@@ -22,16 +26,38 @@ class RegistrationForm(FlaskForm):
     firstName = StringField('First name', validators=[DataRequired()])
     lastName = StringField('Surname', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    username = StringField('Username')
-    password =  PasswordField('Password')
-    pass_confirm =  PasswordField('Confirm password')
+    username = StringField('Username', validators=[DataRequired()])
+    password =  PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords must match!0')])
+    pass_confirm =  PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Register')
+
+    ##  Check if the the email and username hasn't been already taken
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('This email has been already registered.')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('This username has been already taken.')
+
 
 
 ############################################################
 ############   CREATE THE UPDATE THE USER FORM   ###########
 ############################################################
 
-# class UpdateUserForm(FlaskForm):
+##  This form will allow users to change their login credentials if they want to
+class UpdateUserForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    submit = SubmitField('Update')
 
+    ##  Next functions will make sure thet the user will nor enter an email
+    ##  and a username that has been already used
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('This email has been already registered.')
 
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('This username has been already taken.')
